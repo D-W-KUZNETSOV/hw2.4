d484be1f46ce1189298a6074eec1dc82df4aa16b
Dmitryi Kuznetsov <d.w.kuznetsov@yandex.ru>
2024-07-14 19:28:21 +0300

    hw2.4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+
+
 public class Main {
 
   public static void main(String[] args) {
 
+    try {
+      loginPassword.inputCheck("Smitt", "qwertн", "qwerty");
+    } catch (WrongLoginException | WrongPasswordException e) {
+      throw new RuntimeException(e);
+    }
+finally {
+      System.out.println("Код завершён");
+    }
   }
-}
+
+
+  }
+
+
---
+++ src/loginPassword.java
@@ -0,0 +1,20 @@
+import java.util.regex.Pattern;
+
+public class loginPassword {
+
+  private static final Pattern PATTERN = Pattern.compile("^[a-zA-Z0-9_-]{0,20}$");
+
+
+  public static void inputCheck(String login,String password,String confirmPassword) {
+    if (!PATTERN.matcher(login).matches()) {
+      System.out.println("Неправильный логин");
+      throw new WrongLoginException();
+
+  }
+    if (!PATTERN.matcher(password).matches()||!password.equals(confirmPassword)) {
+      System.out.println("Неправильный пароль");
+      throw new WrongPasswordException();
+    }
+  }
+
+}
